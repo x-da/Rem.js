a08ac151df7c18439ec0ce62d16003f029263eb2
小达 <xda05@qq.com>
2016-08-12 15:25:19 +0800

    Update rem.js

--- rem.js
+++ rem.js
@@ -13,27 +13,28 @@
     var win = window;
     var doc = document;
     var dpr = window.devicePixelRatio || 1;
+    var screen = win.screen;
+    //手机宽高比 短:高
+    var ratio = Math.min(screen.width, screen.height) / Math.max(screen.width, screen.height);
     //与FreeUi框架会师
     win.FreeUi = win.FreeUi || {};
     FreeUi['Rem'] = _rem;
 
     function _rem(psd_w, _min, _max, full) {
         var win = window;
-        var screen = win.screen;
-        //手机宽高比 短:高
-        var ratio = Math.min(screen.width, screen.height) / Math.max(screen.width, screen.height);
         //设计稿宽
         var psd_w = Number(psd_w) || 640;
         //手机实际物理像素宽
-        var win_w = win.innerWidth;
+        var win_w = html.getBoundingClientRect().width;
         //短的width
         if (!full) {
-            var angle= window.screen.orientation ? window.screen.orientation.angle : 0;
+            var angle = window.screen.orientation ? window.screen.orientation.angle : 0;
             var orientation = win.orientation || angle || 0;
             if (orientation == 90 || orientation == -90) {
                 //横屏
                 win_w = win_w * ratio;
             };
+
         };
         var size = 100 / (psd_w / win_w);
         var _min = Number(_min) || 50;
@@ -46,16 +47,18 @@
     //立即执行
     var size = _rem(psd_w, _min, _max, full);
     //某些低性能安卓机延迟0.3s执行
-    setTimeout(function() {
+    var _t = setTimeout(function() {
         _rem(psd_w, _min, _max, full);
     }, 300);
+
     //窗口改变
     var is_orientation = 'orientation' in win;
     var event = is_orientation ? 'orientationchange' : 'resize';
-    var time = is_orientation ? 150 : 0;
-    if (!is_orientation) {
+    var time = is_orientation ? 300 : 100;
+    if (!is_orientation || full) {
         win.addEventListener(event, function() {
-            setTimeout(function() {
+            clearTimeout(_t);
+            var _t = setTimeout(function() {
                 _rem(psd_w, _min, _max, full);
             }, time)
         }, false);
@@ -63,6 +66,7 @@
     //窗口显示
     win.addEventListener('pageshow', function() {
         _rem(psd_w, _min, _max, full);
+
     }, false);
     //文档加载完成
     if ("complete" === doc.readyState) {
